527a0de596227fa176226ce35e4708b0da43162e
IIe4enIOshka <g47711@gmail.com>
2021-10-24 20:30:18 +0700

    first commit

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _secondsBetweenSpawn;
+
+    private float _runingTime = 0;
+    private int _currentPoint = 0;
+
+    private void Update()
+    {
+        _runingTime += Time.deltaTime;
+
+        if(_runingTime >= _secondsBetweenSpawn)
+        {
+            _runingTime = 0;
+
+            Instantiate(_template, _spawnPoints[_currentPoint]);
+
+            _currentPoint++;
+
+            if(_currentPoint >= _spawnPoints.Length)
+            {
+                _currentPoint = 0;
+            }
+        }
+    }
+}
